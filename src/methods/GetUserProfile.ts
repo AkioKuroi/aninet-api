0bc5a7060de5f94d4b025b6e2fe663144cfbeeb3
Akio Kuroi <rirukuroi@outlook.com>
2024-08-22 20:30:17 +0300

    Improved security p2 + user profiles

--- src/methods/GetUserProfile.ts
+++ src/methods/GetUserProfile.ts
@@ -1,6 +1,14 @@
 import axios from "axios";
 import { UserProfile } from "../interfaces/UserProfile";
+import createApiMethod from "./createApiMethod";
+
+const BASE_URL = createApiMethod("User");
 
 const GetUserProfile = async (
   userId: number
-): Promise<UserProfile> | null => {};
+): Promise<UserProfile[] | null> => {
+  const profile: UserProfile[] | undefined = await axios
+    .get(`${BASE_URL}?id=${userId}`)
+    .then((response) => response.data);
+  return profile && profile.length > 0 ? profile : null;
+};
