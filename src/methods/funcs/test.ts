274b66c9ac277acf6a7ffd86c7cc51e19800211f
Akio Kuroi <rirukuroi@outlook.com>
2024-08-23 15:07:20 +0300

    Sign In Support

--- src/methods/funcs/test.ts
+++ src/methods/funcs/test.ts
@@ -3,12 +3,13 @@ import { GetUserProfile } from "../GetUserProfile";
 import { ListAnimeById, ListAnimeDefault } from "../ListAnime";
 import { GetRelease } from "../GetRelease";
 import { SignUp } from "../SignUp";
+import { SignIn } from "../SignIn";
 
 const PrintLog = (message: string): void => {
   console.log(`\n\n\n[${new Date().toISOString()}] [LOG] ${message}\n\n\n`);
 };
 
-const createRandomString = (length: number): string {
+const createRandomString = (length: number): string => {
   const chars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
@@ -16,7 +17,7 @@ const createRandomString = (length: number): string {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return result;
-}
+};
 
 (async () => {
   PrintLog("Anime List Test (Guest Mode)");
@@ -42,7 +43,19 @@ const createRandomString = (length: number): string {
   console.log(await GetRelease(54857));
 
   PrintLog("Sign Up Test");
+  const name: string = `Test_${createRandomString(20)}`;
+  console.log(`Name: ${name}`);
   console.log(
-    await SignUp(`Test_${createRandomString(20)}`, "Test_Password", `a${createRandomString(30)}@mail.com`, 1)
+    await SignUp(
+      name,
+      "Test_Password",
+      `a${createRandomString(30)}@mail.com`,
+      1
+    )
+  );
+
+  PrintLog("Sign In Test");
+  console.log(
+    await SignIn("aQ6DeF2RE1iufibKaj3FEKX39SqUZyp@mail.com", "Test_Password")
   );
 })();
