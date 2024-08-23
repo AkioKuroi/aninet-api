213aa55198889d4dcf8da5d5887aec4aea5dfafa
Akio Kuroi <rirukuroi@outlook.com>
2024-08-23 14:51:27 +0300

    Sign Up Support

--- src/methods/funcs/test.ts
+++ src/methods/funcs/test.ts
@@ -2,9 +2,21 @@ import { UserAnimeResponse } from "../../interfaces/ListAnimeTypes";
 import { GetUserProfile } from "../GetUserProfile";
 import { ListAnimeById, ListAnimeDefault } from "../ListAnime";
 import { GetRelease } from "../GetRelease";
+import { SignUp } from "../SignUp";
+
 const PrintLog = (message: string): void => {
   console.log(`\n\n\n[${new Date().toISOString()}] [LOG] ${message}\n\n\n`);
 };
+
+const createRandomString = (length: number): string {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
 
 (async () => {
   PrintLog("Anime List Test (Guest Mode)");
@@ -28,4 +40,9 @@ const PrintLog = (message: string): void => {
 
   PrintLog("Release Test");
   console.log(await GetRelease(54857));
+
+  PrintLog("Sign Up Test");
+  console.log(
+    await SignUp(`Test_${createRandomString(20)}`, "Test_Password", `a${createRandomString(30)}@mail.com`, 1)
+  );
 })();
